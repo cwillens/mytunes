6271ee030a06b8ed1a243b836b634f39f8bf2fec
cwillens <charwillens@gmail.com>
2016-09-28 17:07:20 -0700

    made tests pass for song model and song queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,35 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      //if the added song is the only thing in the queue, run play first
+      // debugger;
+      if (this.at(1) === undefined) {
+        this.playFirst();      
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+      if (this.at(0)) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    }, this);
+
+  },
+
+  playFirst: function() {
+      // make songQueue.at(0).play
+    this.at(0).play();
   }
+  //,
+
+  // add: function() {
+
+  // }
 
 });
